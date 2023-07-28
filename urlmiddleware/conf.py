2323db014edff00712459bbc091acf8c857fc633
David Bramwell <david.bramwell@spongelearning.com>
2023-07-28 14:58:32 +0100

    SPRK-2320: Django 1.10 removed add_prefix from RegexURLPattern

--- urlmiddleware/conf.py
+++ urlmiddleware/conf.py
@@ -11,7 +11,8 @@ def mpatterns(prefix, *args):
         if isinstance(t, (list, tuple)):
             t = middleware(prefix=prefix, *t)
         elif isinstance(t, MiddlewareRegexURLPattern):
-            t.add_prefix(prefix)
+            if not callable(t.callback):
+                t.callback = prefix + '.' + t.callback
         pattern_list.append(t)
     return pattern_list
 
